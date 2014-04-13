1bf0607d1c5a20add274dfd97d9cdd34041c584c
sandykay <skirkoski@icloud.com>
2014-04-13 02:26:39 +0000

    Get the images uploading

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412223605) do
+ActiveRecord::Schema.define(version: 20140413015941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140412223605) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
